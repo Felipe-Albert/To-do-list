1f3823803ac82687c59bf48152925f4f8682ac5d
felipe albert <felipealbertprog@hotmail.com>
2022-04-15 14:40:25 -0300

    Primeiro commit

---
+++ src/components/ListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import Card from "./Card";
+import {deleteItem, changeDone} from '../actions/listAction';
+
+function DoneImg(props){
+
+    if(props.done){
+        return (<img alt="done" src="./assets/on.png"></img>)
+    } else {
+        return (<img alt="undone" src="./assets/off.png"></img>)
+    }
+    
+}
+
+function ListItem(props) {
+    const dispatch = useDispatch()
+    return (<li>
+            <Card className={props.item.done ? "done item" : "item"}>
+                {props.item.text}
+                <div>
+                    <button onClick={() => {dispatch(changeDone(props.item.id))}}><DoneImg done={props.item.done}></DoneImg></button>
+                    <button onClick={()=>{dispatch(deleteItem(props.item.id))}}><img alt="delete" src="./assets/lixeira.png"></img></button>
+                </div>
+            </Card>
+        </li>)
+}
+
+export default ListItem;
